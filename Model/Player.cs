fe0c4e6303d9cf27850da26170f501a84956e22c
RichaSingh00 <ririco613@gmail.com>
2023-09-07 17:51:19 +0530

    Add project files.

---
+++ Model/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp.Model
+{
+    internal class Player
+    {
+        private readonly int _id;
+        private readonly string _name;
+        private readonly int _age;
+        public Player(int id, string name)
+        {
+            _id = id;
+            _name = name;
+        }
+        public Player(int id, string name, int age) : this(id, name)
+        {
+            _age = age;
+        }
+        public int GetId { get { return _id; } }
+        public string GetName { get { return _name; } }
+        public int GetAge { get { return _age; } }
+        public string GetDetails()
+        {
+            return $"Player Id: {_id}\n Player Name: {_name}\n Player Age: {_age}";
+        }
+        public static Player WhoIsElder(Player[] players)
+        {
+            int elderPlayer = players[0]._age;
+            Player p= players[0];
+            for (int i = 1; i < players.Length; i++)
+            {
+                if (players[i]._age > elderPlayer)
+                {
+                    elderPlayer = players[i]._age;
+                    p = players[i];
+                }
+            }
+            return p;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using PlayerApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player[] players =
+            {
+                new Player(1,"Richa",22),
+                new Player(2,"Barbie",23),
+                new Player(3,"Stacy",23),
+                new Player(4,"Jessi",35)
+            };
+            foreach (Player player in players)
+            {
+                Console.WriteLine(player.GetDetails());
+                Console.WriteLine("-----------------------------------------------------------");
+            }
+            Console.WriteLine("The elder amongst the players is: " + Player.WhoIsElder(players).GetDetails());
+
+        }
+    }
+}
